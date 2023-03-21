659fa50bf6c1ff1a831bee06ca62c73925f8feef
vini-nom <vinicius2004gab@gmail.com>
2023-03-21 11:33:10 -0300

    Seletor de Países com login, tela de pesquisa do país e início da tela de informações do país

--- app/src/main/java/com/example/seletorpaises/LoginActivity.kt
+++ app/src/main/java/com/example/seletorpaises/LoginActivity.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -23,8 +24,32 @@ class LoginActivity : AppCompatActivity() {
         btnLogin.setOnClickListener{
             var valorSenha = senhaText.text.toString()
             var valorNome = nomeText.text.toString()
-            finish()
-            startActivity(i)
+
+            var alertDialogView: AlertDialog
+            val builder = AlertDialog.Builder(this)
+
+            if(valorNome == ""){
+                builder.setTitle("Erro")
+                builder.setMessage("Preencha o nome corretamente")
+                alertDialogView = builder.create()
+                alertDialogView.show()
+            }
+            else if(valorSenha == ""){
+                builder.setTitle("Erro")
+                builder.setMessage("Preencha a senha corretamente")
+                alertDialogView = builder.create()
+                alertDialogView.show()
+            }
+            /*else if(valorSenha == "" && valorNome == ""){
+                builder.setTitle("Erro")
+                builder.setMessage("Preencha todos os campos corretamente")
+                alertDialogView = builder.create()
+                alertDialogView.show()
+            }*/
+            else {
+                finish()
+                startActivity(i)
+            }
         }
     }
 }
--- app/src/main/java/com/example/seletorpaises/MainActivity.kt
+++ app/src/main/java/com/example/seletorpaises/MainActivity.kt
@@ -1,12 +1,45 @@
 package com.example.seletorpaises
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import androidx.appcompat.app.AlertDialog
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         supportActionBar?.hide()
+
+        val ListaPaises: List<String> = listOf("Brasil", "Indía", "Holanda", "México", "Papoa Nova Guiné", "Austrália")
+        var btnPesquisa = findViewById<Button>(R.id.btnPesquisaPais)
+        var pais = findViewById<EditText>(R.id.editTextPais)
+
+        btnPesquisa.setOnClickListener{
+            val paisValor = pais.text.toString()
+            val i = Intent(this, PaisesActivity::class.java)
+
+            val alertDialog: AlertDialog
+            val builder = AlertDialog.Builder(this)
+
+            if (paisValor == ""){
+                builder.setTitle("Erro")
+                builder.setMessage("Digite o Nome do País Corretamente")
+                alertDialog = builder.create()
+                alertDialog.show()
+            }
+            else if(paisValor !in ListaPaises){
+                builder.setTitle("Erro")
+                builder.setMessage("O país não foi encontrado, tente pesquisar por outra nação")
+                alertDialog = builder.create()
+                alertDialog.show()
+            }
+            else{
+                finish()
+                startActivity(i)
+            }
+        }
     }
 }
